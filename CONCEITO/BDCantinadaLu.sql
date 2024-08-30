485ecf3789a66be811dcaaa6e06cd3541fba48d0
Guilherme <guilherme.lourenco4239@gmail.com>
2024-08-30 11:28:19 -0300

    BD

---
+++ CONCEITO/BDCantinadaLu.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE bd_cantinadalu;
+
+USE bd_cantinadalu;
+
+-- Ajustando a tabela tb_cliente
+CREATE TABLE tb_cliente (
+ id_cliente INT AUTO_INCREMENT,
+ nome_comp VARCHAR(100),
+ telefone VARCHAR(15),
+ email VARCHAR(100),
+ curso VARCHAR(50),
+ senha VARCHAR(50),
+ tipo VARCHAR(20), 
+ PRIMARY KEY (id_cliente)
+);
+
+-- Ajustando a tabela tb_produto
+CREATE TABLE tb_produto (
+ cod_produto INT AUTO_INCREMENT,
+ nome_produto VARCHAR(100),
+ preco DECIMAL(10,2),
+ url_img VARCHAR(255),
+ descricao TEXT,
+ PRIMARY KEY (cod_produto)
+);
+
+-- Ajustando a tabela tb_avaliacao
+CREATE TABLE tb_avaliacao (
+ id_aval INT AUTO_INCREMENT, 
+ cod_produto INT NOT NULL,  
+ avaliacao DECIMAL(3,2),
+ PRIMARY KEY (id_aval, cod_produto),
+ FOREIGN KEY (cod_produto) REFERENCES tb_produto (cod_produto)
+);
+
+-- Ajustando a tabela tb_carrinho
+CREATE TABLE tb_carrinho (
+ id_carrinho INT AUTO_INCREMENT,
+ cod_produto INT NOT NULL,  -- Mudança de VARCHAR para INT
+ id_cliente INT NOT NULL,
+ PRIMARY KEY (id_carrinho),
+ FOREIGN KEY (cod_produto) REFERENCES tb_produto (cod_produto),
+ FOREIGN KEY (id_cliente) REFERENCES tb_cliente (id_cliente)
+);
+
+-- Ajustando a tabela tb_categoria
+CREATE TABLE tb_categoria (
+ id_categoria INT AUTO_INCREMENT, 
+ cod_produto INT NOT NULL,  -- Mudança de VARCHAR para INT
+ nome VARCHAR(50), 
+ PRIMARY KEY (id_categoria, cod_produto),
+ FOREIGN KEY (cod_produto) REFERENCES tb_produto (cod_produto)
+);
