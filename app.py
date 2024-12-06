922c4e558d4a3cef43965c4103048f69a451e613
KelvynNeris <kelvyn.neris4305@gmail.com>
2024-12-06 08:23:52 -0300

    app

--- app.py
+++ app.py
@@ -1362,5 +1362,5 @@ def usuario(id_cliente):
 
 
 
-
-app.run(debug=True, host="127.0.0.1", port=8080)  # Define o host como localhost e a porta como 8080
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)  # Define o host como localhost e a porta como 8080
